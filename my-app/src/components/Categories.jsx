8af438429fe799601a36216d6084ba03a8acce8a
goldenyak <goldenyakegor@gmail.com>
2021-10-14 22:42:38 +0300

    added some features

--- my-app/src/components/Categories.jsx
+++ my-app/src/components/Categories.jsx
@@ -4,15 +4,13 @@ function Categories({items, onClick}) {
 
     const [activeItem, setActiveItem] = useState(null)
 
-    const onChangeActive = (index) => {
-        setActiveItem(index)
-    }
-
     return (
         <div className="categories">
             <ul>
                 <li className="active">Все</li>
-                {items.map((name, index) => <li className={activeItem === index ? 'active' : ''} key={`${name}_${index}`} onClick={() => onChangeActive(index)}> {name} </li>)}
+                {items.map((name, index) => <li className={activeItem === index ? 'active' : ''}
+                                                key={`${name}_${index}`}
+                                                onClick={() => setActiveItem(index)}> {name} </li>)}
             </ul>
         </div>
     )
