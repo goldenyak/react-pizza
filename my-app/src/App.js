9e8b59026b39367068be8d08f1be8041f2e25916
goldenyak <goldenyakegor@gmail.com>
2021-10-19 21:22:45 +0300

    added some features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import './scss/app.scss';
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
+import {Link, Route} from "react-router-dom";
+
 
 function App() {
 
@@ -12,13 +14,13 @@ function App() {
     return (
         <div className="wrapper">
             <div className="header">
-                <Header/>
+                <Route path="/"><Header/></Route>
             </div>
             <div className="content">
                 <div className="container">
                     <div className="content__top">
                         <Categories items={["Все", "Мясные", "Вегетерианская", "Гриль", "Острые", "Закрытые"]}/>
-                        <Sort items={["популярности", "цене", "алфавиту"]}/>
+                        <Link to="/user"><Sort items={["популярности", "цене", "алфавиту"]}/></Link>
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
