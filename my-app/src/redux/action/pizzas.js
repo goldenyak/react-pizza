6a34b0a3ab3df825c6e3f60de3b7bfa3d04e788f
goldenyak <goldenyakegor@gmail.com>
2021-11-09 22:32:28 +0300

    refactoring code

--- my-app/src/redux/action/pizzas.js
+++ my-app/src/redux/action/pizzas.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-export const setLoaded = (value) => ({
-    type: 'SET_LOADED',
-    payload: value,
-})
 
 export const fetchPizzas = () => (dispatch) => {
     dispatch(setLoaded(false))
     axios.get('http://localhost:3001/pizzas').then(({data}) => {
         dispatch(setPizzas(data))
-    })
-}
+    });
+};
 
+export const setLoaded = (value) => ({
+    type: 'SET_LOADED',
+    payload: value,
+});
 
 const setPizzas = (items) => ({
     type: 'SET_PIZZAS',
     payload: items,
-})
+});
 
 export default setPizzas;
