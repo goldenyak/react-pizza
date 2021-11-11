e7b3787d260e9998200946ad3a55301d68476288
goldenyak <goldenyakegor@gmail.com>
2021-11-11 21:43:24 +0300

    refactoring code

---
+++ my-app/src/redux/reducers/cart.js
@@ -0,0 +1,29 @@
+const initialState = {
+    items: {},
+    totalPrice: 0,
+    totalCount: 0,
+}
+
+
+const pizzas = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_TOTAL_PRICE':
+            return {
+                ...state,
+                totalPrice: action.payload,
+
+            };
+
+        case 'SET_TOTAL_COUNT':
+            return {
+                ...state,
+                totalCount: action.payload,
+
+            };
+
+        default: return state;
+    }
+}
+
+
+export default pizzas;
